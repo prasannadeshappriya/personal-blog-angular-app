62c5d1e5dc34074187de071f920d6fc8ac7195ec
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2020-05-13 08:23:53 +0530

    minor changes

--- src/app/config/app.config.js
+++ src/app/config/app.config.js
@@ -1,5 +1,5 @@
 export const BLOG_CONSTANTS = {
   DEBUG_ENABLE: false,
-  EMAIL_SERVICE_DEBUG_URL: 'http://localhost:3000/api/send-email-debug',
-  EMAIL_SERVICE_URL: 'http://localhost:3000/api/send-email'
+  EMAIL_SERVICE_DEBUG_URL: 'https://prasanna.alwaysdata.net/api/send-email-debug',
+  EMAIL_SERVICE_URL: 'https://prasanna.alwaysdata.net/api/send-email'
 }
