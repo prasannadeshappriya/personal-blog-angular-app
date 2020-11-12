db696901618344edc3771dca974a45ef00a313fc
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2020-11-13 00:27:28 +0530

    Minor changes

--- src/app/config/app.config.js
+++ src/app/config/app.config.js
@@ -1,7 +1,7 @@
 export const CONSTANTS = {
   DEBUG_ENABLE: false,
-  URL_DOMAIN: 'http://localhost:3000/',
-  // URL_DOMAIN: 'https://prasanna.alwaysdata.net/',
+  // URL_DOMAIN: 'http://localhost:3000/',
+  URL_DOMAIN: 'https://prasanna.alwaysdata.net/',
 
   EMAIL_SERVICE_DEBUG_URL: 'api/send-email-debug',
   EMAIL_SERVICE_URL: 'api/send-email',
@@ -13,5 +13,5 @@ export const CONSTANTS = {
   STATISTICS_CREATE_URL: 'stat/create-stat',
   STATISTICS_COUNT: 'stat/get-all-stat-record-count',
 
-  IP_RETREVE_URL: 'http://api.ipify.org/?format=json'
+  IP_RETREVE_URL: 'https://api.ipify.org/?format=json'
 }
